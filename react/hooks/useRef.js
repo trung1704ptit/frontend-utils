3386571d8b81bf2503c13d30da83120c1aa68053
Trung Nguyen <trung1704ptit@gmail.com>
2022-05-28 10:39:33 +0700

    add example useRef

--- react/hooks/useRef.js
+++ react/hooks/useRef.js
@@ -1,3 +1,20 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
+
+// Example 1:
 const refContainer = useRef(initialValue);
+
+// -------------------------------------------------------
+// Example 2:
+const App = () => {
+  const fetchAPI = useRef(() => { });
+
+  fetchAPI.current = async () => {
+    const { data } = await axios.get('url');
+    return data;
+  }
+
+  useEffect(() => {
+    fetchAPI.current()
+  }, [])
+}
